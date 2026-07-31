fc7350047c18a83e5822e10c70978bf4c918fc78
tarang54-tech <sritarangpeddaboina@gmail.com>
2026-07-31 18:57:31 +0530

    Fix visit import

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const Book = require("./models/Book");
-const Visit = require("./models/Visit");
+const Visit = require("./models/visit");
 require("dotenv").config();
 
 const app = express();
